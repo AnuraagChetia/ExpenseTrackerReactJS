aae7c80a72bbda44b28d91672e9f6d7792505462
AnuraagChetia <anuraagchetia77@gmail.com>
2023-03-07 20:59:19 +0530

    first push

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Tracker!</h2>
+      <h3>Expense Items</h3>
+      <ul>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+      </ul>
+    </div>
+  );
+}
+export default ExpenseItem;
