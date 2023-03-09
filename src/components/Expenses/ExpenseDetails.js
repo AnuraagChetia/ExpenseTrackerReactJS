3e76d74cce244ee97f513e920dcf108cdddc1a62
AnuraagChetia <anuraagchetia77@gmail.com>
2023-03-09 21:42:06 +0530

    Manipulating the DOM - React States

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,9 +1,22 @@
 import "./ExpenseItem.css";
+import React, { useState } from "react";
 const ExpenseDetails = (props) => {
+  const [title,setTitle] = useState(props.details.title);
+  const [expense,setExpense] = useState(props.details.amount);
+  const clickHandler = () =>{
+    setTitle('Updated!!!')
+    console.log(title);
+  }
+  const changeExpense = () =>{
+    setExpense('100');  
+    console.log(expense);
+  }
   return (
     <div className="expense-item__description">
-      <h2>{props.details.title}</h2>
-      <div className="expense-item__price">${props.details.amount}</div>
+      <h2>{title}</h2>
+      <div className="expense-item__price">${expense}</div>
+      <button onClick={clickHandler}>Delete</button>
+      <button onClick={changeExpense}>Amount</button>
     </div>
   );
 };
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,10 @@ import React from "react";
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseDetails from "./ExpenseDetails";
 const ExpenseItem = (props) => {
-  const clickHandler = () =>{
-    console.log('Deleted')
-  }
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetails details={props}></ExpenseDetails>
-      <button id="btn" onClick={clickHandler}>Delete</button>
     </div>
     
   );
