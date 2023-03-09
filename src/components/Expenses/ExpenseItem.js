3f386440580c8f6cec367a367bfcab4e98fff8d0
AnuraagChetia <anuraagchetia77@gmail.com>
2023-03-09 20:29:35 +0530

    Adding Event Listeners made easy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,11 +3,16 @@ import React from "react";
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseDetails from "./ExpenseDetails";
 const ExpenseItem = (props) => {
+  const clickHandler = () =>{
+    console.log('Deleted')
+  }
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetails details={props}></ExpenseDetails>
+      <button id="btn" onClick={clickHandler}>Delete</button>
     </div>
+    
   );
 };
 export default ExpenseItem;
