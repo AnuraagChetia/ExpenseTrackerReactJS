594c034e72029194aaac4017de4287801f716ecc
AnuraagChetia <anuraagchetia77@gmail.com>
2023-03-14 22:29:14 +0530

    Conditional Rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,20 +10,30 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
   const filteredExpenses = props.items.filter(expense=>{return expense.date.getFullYear().toString()===filteredYear}) 
+
+  let expenseContent = <p>No expenses found</p>
+  if(filteredExpenses.length > 0){
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      ></ExpenseItem>
+    ))
+  }
+  if(filteredExpenses.length===1){
+    expenseContent.push(<p key="null" >Only single Expense here. Please add more...</p>)
+  }
+
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        ></ExpenseItem>
-      ))}
+      {expenseContent}
     </Card>
   );
 };
